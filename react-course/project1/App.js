8035370bdd22cf6dcc24d5f9879fd5bd7324ed77
J.A. Rodrigo Puca <r@geeks.ms>
2020-03-18 22:15:14 -0300

    3/18: hi TS!

--- react-course/project1/App.js
+++ react-course/project1/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Picker } from 'react-native';
 
 
 /**
@@ -14,37 +14,37 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 class Timer extends React.Component {
   constructor() {
     super();
-    this.state = { time: 0, active:false, break:true}
+    this.state = { time: 0, active: false, break: true, tWork:0, tBreak:0 }
     this.active = false;
     this.interval = null;
   }
 
   decrement = () => {
-    if (this.active){
+    if (this.active) {
       if (this.state.time > 0) {
         this.setState(prev => ({ time: prev.time - 1 }));
       }
-      else{
-        let newTime=this.state.break?10:5;
-        this.setState(prev=>({ time: newTime, break:!prev.break }))
+      else {
+        let newTime = this.state.break ? this.state.tWork : this.state.tBreak;
+        this.setState(prev => ({ time: newTime, break: !prev.break }))
       }
     }
-    
+
   }
 
 
   start = () => {
-    this.active=true;
+    this.active = true;
+    clearInterval(this.interval);
     this.interval = setInterval(this.decrement, 1000);
-
   }
 
   stop = () => {
     this.active = false;
   }
 
-  reset = () =>{
-    this.setState({ time: 0, active:false, break:true})
+  reset = () => {
+    this.setState({ time: 0, active: false, break: true })
   }
 
   componentWillUnmount() {
@@ -57,7 +57,29 @@ class Timer extends React.Component {
         <Text>{Math.floor(this.state.time / 60)}:{Math.floor(this.state.time % 60)}</Text>
         <Button title="Start" onPress={this.start} />
         <Button title="Stop" onPress={this.stop} />
-        <Button title="Reset" onPress={this.reset}/>
+        <Button title="Reset" onPress={this.reset} />
+
+        <Picker
+          selectedValue={this.state.tWork}
+          style={{ height: 50, width: 100 }}
+          onValueChange={(itemValue, itemIndex) =>
+            this.setState({ tWork: itemValue })
+          }>
+          <Picker.Item label="2 min" value="120" />
+          <Picker.Item label="1 min" value="60" />
+        </Picker>
+
+        <Picker
+          selectedValue={this.state.tBreak}
+          style={{ height: 50, width: 100 }}
+          onValueChange={(itemValue, itemIndex) =>
+            this.setState({ tBreak: itemValue })
+          }>
+          <Picker.Item label="2 min" value="120" />
+          <Picker.Item label="1 min" value="60" />
+        </Picker>
+
+
       </View>
     )
   }
@@ -71,7 +93,6 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
         <Timer />
       </View>
     );
