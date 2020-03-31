2cab4cda7b9dc481fba089b4fe8dbe6a9906b4a7
J.A. Rodrigo Puca <r@geeks.ms>
2020-03-31 03:09:11 -0300

    register

--- react-course/project1/web-build/register-service-worker.js
+++ react-course/project1/web-build/register-service-worker.js
@@ -1,1 +1,14 @@
-// noop
+/* eslint-env browser */
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker
+      .register('/pomodoro/expo-service-worker.js', { scope: '/pomodoro/' })
+      .then(function(info) {
+        // console.info('Registered service-worker', info);
+      })
+      .catch(function(error) {
+        console.info('Failed to register service-worker', error);
+      });
+  });
+}
