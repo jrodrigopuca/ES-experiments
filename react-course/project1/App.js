5dd4566621aa3b03efcd98e35dafdf88c0b713aa
J.A. Rodrigo Puca <r@geeks.ms>
2020-03-31 03:04:50 -0300

    3/31: pomodoro

--- react-course/project1/App.js
+++ react-course/project1/App.js
@@ -14,7 +14,7 @@ import { StyleSheet, Text, View, Button, Picker } from 'react-native';
 class Timer extends React.Component {
   constructor() {
     super();
-    this.state = { time: 0, active: false, break: true, tWork:0, tBreak:0 }
+    this.state = { time: 0, break: true, isWorking: false, tWork: 1500, tBreak: 300 }
     this.active = false;
     this.interval = null;
   }
@@ -29,57 +29,91 @@ class Timer extends React.Component {
         this.setState(prev => ({ time: newTime, break: !prev.break }))
       }
     }
-
   }
 
-
   start = () => {
+    this.setState({ isWorking: true });
     this.active = true;
     clearInterval(this.interval);
     this.interval = setInterval(this.decrement, 1000);
   }
 
-  stop = () => {
-    this.active = false;
+  resume = () => {
+    this.active = !this.active;
   }
 
   reset = () => {
-    this.setState({ time: 0, active: false, break: true })
+    this.setState({ time: 0, break: true })
   }
+
+  back=()=>{
+    this.setState({ time: 0, break: true, isWorking:false})
+    this.active = false;  
+  }
+
 
   componentWillUnmount() {
     clearInterval(this.interval);
   }
 
+  makeGoodNumber(number){
+    return number<10?"0"+number:number;
+  }
+
+  myStyle() {
+    return {
+      backgroundColor: this.state.break ? "#ff5959" : "#4f9da6",
+      margin:0,
+      flex:1,
+      width: "100%",
+      alignItems: "center",
+      justifyContent: 'center'
+    }
+  }
+
   render() {
+    const isWorking = this.state.isWorking;
     return (
-      <View>
-        <Text>{Math.floor(this.state.time / 60)}:{Math.floor(this.state.time % 60)}</Text>
-        <Button title="Start" onPress={this.start} />
-        <Button title="Stop" onPress={this.stop} />
-        <Button title="Reset" onPress={this.reset} />
+      <View style={this.myStyle()}>
+        {isWorking &&
+          <View>
+            <Text style={{fontSize:78}}>{this.makeGoodNumber(Math.floor(this.state.time / 60))}:{this.makeGoodNumber(Math.floor(this.state.time % 60))}</Text>
+            <Button title="Resume/Stop" onPress={this.resume} color="#1a0841"/>
+            <Button title="Reset" onPress={this.reset} color="#1a0841"/>
+            <Button title="Back" onPress={this.back} color="#1a0841"/>
+          </View>}
 
-        <Picker
-          selectedValue={this.state.tWork}
-          style={{ height: 50, width: 100 }}
-          onValueChange={(itemValue, itemIndex) =>
-            this.setState({ tWork: itemValue })
-          }>
-          <Picker.Item label="2 min" value="120" />
-          <Picker.Item label="1 min" value="60" />
-        </Picker>
+        {!isWorking &&
+          <View>
+            <Text>Work time:</Text>
+            <Picker
+              selectedValue={this.state.tWork}
+              style={{ height: 50, width: 100 }}
+              onValueChange={(itemValue, itemIndex) =>
+                this.setState({ tWork: itemValue })
+              }>
+              <Picker.Item label="25 min" value={1500} />
+              <Picker.Item label="30 min" value={1800} />
+              <Picker.Item label="20 min" value={1200} />
+              <Picker.Item label="10 min" value={600} />
+              
+              
+            </Picker>
 
-        <Picker
-          selectedValue={this.state.tBreak}
-          style={{ height: 50, width: 100 }}
-          onValueChange={(itemValue, itemIndex) =>
-            this.setState({ tBreak: itemValue })
-          }>
-          <Picker.Item label="2 min" value="120" />
-          <Picker.Item label="1 min" value="60" />
-        </Picker>
-
-
+            <Text>Break Time:</Text>
+            <Picker
+              selectedValue={this.state.tBreak}
+              style={{ height: 50, width: 100 }}
+              onValueChange={(itemValue, itemIndex) =>
+                this.setState({ tBreak: itemValue })
+              }>
+              <Picker.Item label="5 min" value={300} />
+              <Picker.Item label="3 min" value={180} />
+              <Picker.Item label="2 min" value={120} />
+              <Picker.Item label="1 min" value={60} />
+            </Picker>
+            <Button title="Start" onPress={this.start} color="#ffad5a" />
+          </View>}
       </View>
     )
   }
@@ -102,8 +136,9 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    padding:0,
+    margin:0
+  }
 });
