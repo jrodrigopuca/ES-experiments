368054ec19fd862a97b8d43b569285dcb0f304f7
J.A. Rodrigo Puca <r@geeks.ms>
2020-04-04 16:31:12 -0300

    todo list with local storage

--- react-course/project0/react/script3.js
+++ react-course/project0/react/script3.js
@@ -3,13 +3,14 @@ const e= React.createElement;
 const Todo = props => {return (
     <li key={props.todo.id}>
         <span>[{props.todo.id}]: </span>
-        <span>{props.todo.text}</span>
+        <span className="todo-text">{props.todo.text}</span>
         <input
             type="checkbox"
+            className="todo-checkbox"
             checked={props.todo.checked}
             onChange={props.onToggle}
         />
-        <button onClick={props.onDelete}> delete </button>
+        <button className="todo-delete" onClick={props.onDelete}> delete </button>
     </li>
 )};
 
@@ -24,11 +25,17 @@ class App extends React.Component {
         };
     }
 
+    componentDidMount(){
+        const local=JSON.parse(localStorage.getItem('todos'));
+        if (local != null) this.setState({todos:local})
+    }
+
     addTodo() {
         const text = prompt("text please!");
         this.setState({
             todos: [...this.state.todos, { id: id++, text: text, checked: false }]
         });
+        //localStorage.setItem('todos', JSON.parse(this.state.todos));
     }
 
     toggleTodo(id) {
@@ -38,23 +45,33 @@ class App extends React.Component {
                 return { id: todo.id, text: todo.text, checked: !todo.checked };
             })
         });
+        //localStorage.setItem('todos', this.state.todos);
     }
 
     removeTodo(id) {
         this.setState({
             todos: this.state.todos.filter(todo => todo.id !== id)
         });
+        //localStorage.setItem('todos', this.state.todos);
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        localStorage.setItem('todos', JSON.stringify(nextState.todos));
+        return true;
     }
 
     render() {
         return (
-            <div>
-                <p> cantidad :{this.state.todos.length}</p>
-                <p>
-                    pendientes: {this.state.todos.filter(todo => !todo.checked).length}
+            <div className="container center">
+                <h1 className="center title">My TODO App</h1>
+                <div className="flow-right controls">
+                <p id="item-count"> Item count :{this.state.todos.length}</p>
+                <p id="unchecked-count">
+                    Unchecked count: {this.state.todos.filter(todo => !todo.checked).length}
                 </p>
-                <button onClick={() => this.addTodo()}> addTodo </button>
-                <ul>
+                </div>
+                <button className="button center" onClick={() => this.addTodo()}> New To-do </button>
+                <ul id="todo-list" className="todo-list">
                     {this.state.todos.map(todo => (
                         <Todo
                             todo={todo} key={todo.id}
